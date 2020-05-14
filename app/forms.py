86bf2f1ec1aeb81d1e986943fd09335140ff0b87
Emiel Zyde <emiel@mosaicsmartdata.com>
2020-05-14 21:17:52 +0200

    Added code for Flask forms and HTML templates rendered by the API

---
+++ app/forms.py
@@ -0,0 +1,61 @@
+from functools import lru_cache
+
+from flask_wtf import FlaskForm
+from wtforms import RadioField, SelectField, SubmitField, SelectMultipleField
+
+from covid_analysis.data_utils import fetch_latest_cases_data, preprocess_covid_data
+
+YES_NO_CHOICES = [('Yes', 'Yes'), ('No', 'No')]
+
+
+@lru_cache(maxsize=1)
+def get_available_countries():
+    covid_data = fetch_latest_cases_data()
+    covid_data = preprocess_covid_data(covid_data)
+    return list(covid_data.index)
+
+
+class CountrySelectionForm(FlaskForm):
+    countries = get_available_countries()
+    country_choices = [tuple([country, country]) for country in countries]
+    country = SelectField('Country', choices=country_choices)
+    submit = SubmitField('Submit selection!')
+
+
+class CountryDataFormatForm(FlaskForm):
+    daily_changes = RadioField('Daily changes', choices=YES_NO_CHOICES, default='Yes')
+    rolling_average = RadioField(
+        'Rolling average',
+        choices=YES_NO_CHOICES,
+        default='Yes',
+    )
+    population_normaliser = RadioField(
+        'Normalize by population',
+        choices=YES_NO_CHOICES,
+        default='Yes'
+    )
+    submit = SubmitField('Submit selection!')
+
+
+class PeakPredictionSelectionForm(CountryDataFormatForm):
+    available_countries = get_available_countries()
+    country_choices = [tuple([country, country]) for country in available_countries]
+    countries = SelectMultipleField('Countries to plot', choices=country_choices)
+
+
+class MultipleCountryDataForm(CountryDataFormatForm):
+    adjust_for_time_diff = RadioField(
+        'Adjust for time differences',
+        choices=YES_NO_CHOICES,
+        default='Yes',
+    )
+    types_of_data = [
+        ('infections', 'Infections'),
+        ('deaths', 'Deaths'),
+        ('recoveries', 'Recoveries')
+    ]
+    data_type = RadioField(
+        'Type of data',
+        choices=types_of_data,
+        default='infections',
+    )
